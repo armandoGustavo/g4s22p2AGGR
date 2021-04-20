d8bbc4a42db761f1ce3ca99b707b090102571ab1
arman <arman@LAPTOP-MIEMD7VH>
2021-04-20 17:22:11 -0500

    Practica 2

---
+++ src/g4s22p2aggr/G4s22p2AGGR.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package g4s22p2aggr;
+
+/**
+ *
+ * @author arman
+ */
+public class G4s22p2AGGR {
+
+   
+    public static void main(String[] args) {
+        FrmPrincipal frmPrincipal = new FrmPrincipal();
+        frmPrincipal.setVisible(true);
+    }
+    
+}
